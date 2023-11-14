db0f99add226828f428517c0dd545c8c61bf9762
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-14 22:46:48 +0100

    initial commit

---
+++ app/src/main/java/com/omarcomputer/phonecontactaccess/Contact.java
@@ -0,0 +1,35 @@
+package com.omarcomputer.phonecontactaccess;
+
+public class Contact {
+    String name;
+    String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/omarcomputer/phonecontactaccess/MainActivity.java
@@ -0,0 +1,76 @@
+package com.omarcomputer.phonecontactaccess;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import com.omarcomputer.phonecontactaccess.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayList<Contact> contactList = new ArrayList<>();
+    int index = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getContacts();
+        showContact();
+
+        binding.btnPrevious.setOnClickListener(v -> {
+            if (index > 0 && contactList.size() > 0) {
+                index--;
+                showContact();
+            }
+        });
+        binding.btnNext.setOnClickListener(v -> {
+            if (index < contactList.size() -1) {
+                index++;
+                showContact();
+            }
+        });
+    }
+
+    private void showContact() {
+        Contact contact = contactList.get(index);
+        binding.txtName.setText(contact.name);
+        binding.txtPhoneNumber.setText(contact.phoneNumber);
+        binding.txtCurrentContact.setText(""+ index + "/"+ contactList.size());
+    }
+
+    private void getContacts() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},100);
+            return;
+        }
+        contactList.clear();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+        if(cursor !=null) {
+            try {
+                while(cursor.moveToNext()) {
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactList.add(new Contact(name, phoneNumber));
+                }
+            } catch (Exception e) {
+
+            } finally {
+                cursor.close();
+            }
+        }
+    }
+
+}
